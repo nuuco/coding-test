8b01478d67afbaa69c642143bd0b482e886051d0
nuuco <nuuco.coding@gmail.com>
2022-06-05 01:21:06 +0900

    해결 완료

---
+++ baekjoon_level_1/06_A-B/no_1001.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split(' ');
+
+const a = parseInt(input[0]);
+const b = parseInt(input[1]);
+console.log(a - b);
