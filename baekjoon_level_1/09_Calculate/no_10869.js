c2bd9facd91a8089a11e64e448b66cc185a0d3a0
nuuco <nuuco.coding@gmail.com>
2022-06-06 01:41:13 +0900

    해결 완료

---
+++ baekjoon_level_1/09_Calculate/no_10869.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split(' ');
+
+const num1 = parseInt(input[0]);
+const num2 = parseInt(input[1]);
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(parseInt(num1 / num2));
+console.log(num1 % num2);
