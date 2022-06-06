6e910b44df1dfc7960a6bf27051c802f0ea47d81
nuuco <nuuco.coding@gmail.com>
2022-06-07 00:13:05 +0900

    해결 완료

---
+++ baekjoon_level_1/11_ThaiYear/no_18108.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
+const input = fs.readFileSync(filePath).toString().trim();
+
+console.log(parseInt(input) - 543);
