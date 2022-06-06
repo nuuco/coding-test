33833a1638105cb42fe24f21d2fe4bf419d70a9d
nuuco <nuuco.coding@gmail.com>
2022-06-07 00:13:13 +0900

    해결 완료

---
+++ baekjoon_level_1/12_Remainder/no_10430.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split(' ');
+const [A, B, C] = input.map(el => parseFloat(el));
+
+console.log((A + B) % C);
+console.log(((A % C) + (B % C)) % C);
+console.log((A * B) % C);
+console.log(((A % C) * (B % C)) % C);
