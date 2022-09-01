c0886c48f108b38131d44b358eef01a581e1430d
nuuco <nuuco.coding@gmail.com>
2022-09-02 01:52:14 +0900

    해결 완료

---
+++ Class_1/09_PrintAsItIs/no_11718.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
+
+const input = fs.readFileSync(filePath).toString().trim();
+
+console.log(input);
