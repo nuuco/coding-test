bc124df0a43b79ca196adf7125f8d2ca45064a5a
nuuco <nuuco.coding@gmail.com>
2022-10-12 13:10:20 +0900

    해결 완료

---
+++ Class_1/24_PrintStar13/no_2523.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
+
+const n = Number(fs.readFileSync(filePath).toString().trim());
+const result = [];
+for(let i = 1; i <= n; i++) {
+  const tmp = '*'.repeat(i);
+  result.push(tmp);
+}
+for(let i = n - 1; i >= 1; i--) {
+  const tmp = '*'.repeat(i);
+  result.push(tmp);
+}
+
+console.log(result.join('\n'));
