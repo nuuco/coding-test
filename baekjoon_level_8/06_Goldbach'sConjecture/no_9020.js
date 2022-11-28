c3b0e772055695f3f300399a64747ba037559860
nuuco <nuuco.coding@gmail.com>
2022-11-28 18:46:02 +0900

    feat: 주석 수정

--- baekjoon_level_8/06_Goldbach'sConjecture/no_9020.js
+++ baekjoon_level_8/06_Goldbach'sConjecture/no_9020.js
@@ -1,22 +1,29 @@
-const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
-const [n, ...arr] = fs.readFileSync(filePath).toString().trim().split('\n').map(el => Number(el));
+const fs = require("fs");
+const filePath =
+  process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt";
+const [n, ...arr] = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map((el) => Number(el));
 
+//골드바흐의 추측
 //에라토스테네스의 체 로 소수 미리 걸러내기
-const max = Math.max(...arr); 
+const max = Math.max(...arr);
 
 const primeArr = Array(max + 1).fill(true);
 primeArr[0] = false;
 primeArr[1] = false;
 
-for(let i = 2; i <= Math.floor(Math.sqrt(max)); i++) {
-    if(primeArr[i]) {
-        let j = 2;
-        while(i * j <= max) {
-            primeArr[i * j] = false;
-            j++;
-        }
+for (let i = 2; i <= Math.floor(Math.sqrt(max)); i++) {
+  if (primeArr[i]) {
+    let j = 2;
+    while (i * j <= max) {
+      primeArr[i * j] = false;
+      j++;
     }
+  }
 }
 //각 인덱스 번호의 숫자가 소수인지 여부를 true, false 로 저장하는 배열 완성
 /** max가 16일때
@@ -29,16 +36,13 @@ for(let i = 2; i <= Math.floor(Math.sqrt(max)); i++) {
 ]
 */
 
-for(let el of arr) {
-    let half = parseInt(el / 2);
-    while(half > 1) {
-        if(primeArr[half] && primeArr[el - half]) {
-            break;
-        }
-        half--;
+for (let el of arr) {
+  let half = parseInt(el / 2);
+  while (half > 1) {
+    if (primeArr[half] && primeArr[el - half]) {
+      break;
     }
-    console.log(`${half} ${el - half}`);
+    half--;
+  }
+  console.log(`${half} ${el - half}`);
 }
-
-
-
