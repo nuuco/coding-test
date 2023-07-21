a06b6b0eb2449f8c2bf19029cc3a6bf9441e7dbb
nuuco <nuuco.coding@gmail.com>
2023-07-21 22:47:22 +0900

    해결 완료

---
+++ etc_problems/232_A+B-6/no_4592.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const filePath =
+  process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt";
+
+const [t, ...input] = fs.readFileSync(filePath).toString().trim().split('\n');
+const result = [];
+
+for(let el of input) {
+  const [a, b] = el.split(',').map(Number);
+  result.push(a + b);
+}
+
+console.log(result.join('\n'));
