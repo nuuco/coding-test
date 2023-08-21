befc145854698b2d68010761e1e4f7bffd1ae90d
nuuco <nuuco.coding@gmail.com>
2023-08-22 07:10:43 +0900

    해결 완료

---
+++ etc_problems/263_그대로 출력하기2/no_4592.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const filePath =
+  process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt";
+
+const input = fs.readFileSync(filePath).toString();
+
+
+console.log(input);
