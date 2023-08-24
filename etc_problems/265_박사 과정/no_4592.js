79749ab7fee98906f1b0f94bc30e5b987833da54
nuuco <nuuco.coding@gmail.com>
2023-08-24 18:11:13 +0900

    해결 완료

---
+++ etc_problems/265_박사 과정/no_4592.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const filePath =
+  process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt";
+
+const [t, ...input] = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const result = [];
+
+for (let el of input) {
+  if (el === "P=NP") {
+    result.push("skipped");
+  } else {
+    const [a, b] = el.split("+").map(Number);
+    result.push(a + b);
+  }
+}
+
+console.log(result.join("\n"));
