d819189bcf84c0800442a587c591422319648c72
nuuco <nuuco.coding@gmail.com>
2023-09-18 20:19:34 +0900

    해결 완료

---
+++ etc_problems/289_콜센터/no_4592.js
@@ -0,0 +1,14 @@
+const result = [];
+
+result.push(`     /~\\`);
+result.push(`    ( oo|`);
+result.push(`    _\\=/_`);
+result.push(`   /  _  \\`);
+result.push(`  //|/.\\|\\\\`);
+result.push(` ||  \\ /  ||`);
+result.push(`============`);
+result.push(`|          |`);
+result.push(`|          |`);
+result.push(`|          |`);
+
+console.log(result.join("\n"));
