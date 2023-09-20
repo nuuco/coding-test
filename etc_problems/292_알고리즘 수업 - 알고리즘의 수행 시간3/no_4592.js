137ea6592a6155c567dad64db87a32b81efe24ed
nuuco <nuuco.coding@gmail.com>
2023-09-21 03:14:37 +0900

    해결 완료

---
+++ etc_problems/292_알고리즘 수업 - 알고리즘의 수행 시간3/no_4592.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt";
+
+const n = Number(fs.readFileSync(filePath).toString().trim());
+
+console.log(n ** 2);
+console.log(2);
+
