740b1900f12bdd26f2364e2ac4f6375bb349894a
nuuco <nuuco.coding@gmail.com>
2023-11-10 17:54:34 +0900

    해결 완료

---
+++ etc_problems/344_골뱅이 찍기 돌아간 ㄹ/no_17362.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt";
+
+const n = Number(fs.readFileSync(filePath).toString().trim());
+
+const result = [];
+for (let i = 1; i <= 5 * n; i++) {
+  if (i <= n) {
+    result.push("@".repeat(3 * n) + " ".repeat(n) + "@".repeat(n));
+  } else if (i > 5 * n - n) {
+    result.push("@".repeat(n) + " ".repeat(n) + "@".repeat(3 * n));
+  } else {
+    result.push("@".repeat(n) + " ".repeat(n) + "@".repeat(n) + " ".repeat(n) + "@".repeat(n));
+  }
+}
+
+console.log(result.join("\n"));
