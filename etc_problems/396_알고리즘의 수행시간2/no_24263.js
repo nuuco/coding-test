19515d977aec1db6e8873b6efe359a9cb085ee1c
nuuco <nuuco.coding@gmail.com>
2024-01-02 10:04:23 +0900

    해결 완료

---
+++ etc_problems/396_알고리즘의 수행시간2/no_24263.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt";
+
+const n = Number(fs.readFileSync(filePath).toString().trim());
+
+console.log(n);
+console.log(1);
