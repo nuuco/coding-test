4615d32b9d379387292662ad0a6b4366d6e4a0ae
nuuco <nuuco.coding@gmail.com>
2024-01-31 16:37:16 +0900

    해결 완료

---
+++ etc_problems/425_삼각무늬2/no_11312.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt";
+
+const [t, ...input] = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const result = [];
+for (let el of input) {
+  const [a, b] = el.split(" ").map(Number);
+  const slide = Math.ceil(a / b);
+  result.push(slide ** 2);
+}
+
+console.log(result.join("\n"));
