6c6dbe5b018cb14f3b3df33a0b8258bf744f5130
nuuco <nuuco.coding@gmail.com>
2024-02-26 03:08:22 +0900

    해결 완료

---
+++ etc_problems/451_LivestockCount/no_4589.js
@@ -0,0 +1,22 @@
+console.log(`Animal      Count
+-----------------
+Chickens      100
+Clydesdales     5
+Cows           40
+Goats          22
+Steers          2`);
+
+
+////
+
+
+// with Ada.Text_IO;
+// procedure Hello is begin
+//     Ada.Text_IO.Put_Line("Animal      Count");
+//     Ada.Text_IO.Put_Line("-----------------");
+//     Ada.Text_IO.Put_Line("Chickens      100");
+//     Ada.Text_IO.Put_Line("Clydesdales     5");
+//     Ada.Text_IO.Put_Line("Cows           40");
+//     Ada.Text_IO.Put_Line("Goats          22");
+//     Ada.Text_IO.Put_Line("Steers          2");
+// end Hello;
