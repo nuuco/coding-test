d0daf45df0dcf1bee84a48d6d07fe8d659d4a544
nuuco <nuuco.coding@gmail.com>
2024-05-11 20:06:39 +0900

    해결 완료

---
+++ etc_problems/526_직사각형/no_27323.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt";
+
+const [a, b] = fs.readFileSync(filePath).toString().trim().split("\n").map(Number);
+
+console.log(a * b);
